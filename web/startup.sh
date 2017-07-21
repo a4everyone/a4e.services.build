cd4571ae8e4b25065ac87f50c1b57f28022a9ef8
Nikolay Nikolov <nikolay.nikolov@a4everyone.com>
2017-07-21 15:49:14 +0300

    initial commit

---
+++ web/startup.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+echo penil
+cat /home/nginx.conf.tmpl | envsubst \'$(env | grep ^NG_ | awk 'BEGIN{FS="=";ORS="";}{print "$"$1}')\' > /etc/nginx/conf.d/nginxa4e.conf
+
+exec nginx -g "daemon off;"
